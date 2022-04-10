ce496b84f04b614d147206f2b03f4b1e7ffb5912
Hossen <mdmitho199911@gmail.com>
2022-04-10 15:47:49 +0600

    create require auth and navigate from location

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from './components/Header/Header';
 import Inventory from './components/Inventory/Inventory';
 import Login from './components/Login/Login';
 import Orders from './components/Orders/Orders';
+import RequierAuth from './components/RequierAuth/RequierAuth';
 import Shop from './components/Shop/Shop';
 import SingUP from './components/SingUp/SingUP';
 
@@ -17,7 +18,11 @@ function App() {
         <Route path='/' element={<Shop></Shop>}></Route>
         <Route path='/shop' element={<Shop></Shop>}></Route>
         <Route path='/orders' element={<Orders></Orders>}></Route>
-        <Route path='/inventory' element={<Inventory></Inventory>}></Route>
+        <Route path='/inventory' element={
+        <RequierAuth>
+          <Inventory></Inventory>
+        </RequierAuth>
+      }></Route>
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/signup' element={<SingUP></SingUP>}></Route>
